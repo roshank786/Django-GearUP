3c05d526fca3876a045e25267724bc3791613271
Amal Roshan <amalroshank786@gmail.com>
2024-08-07 14:47:20 +0530

    Update 'myapp' in settings.py & create Car and Brand(Models)

--- myapp/models.py
+++ myapp/models.py
@@ -2,3 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+
+class Brand(models.Model):
+     
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+
+
+class Car(models.Model):
+
+    company = models.ForeignKey(Brand,on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    year = models.PositiveIntegerField()
+    fuel = models.CharField(max_length=50)
+    transmission = models.CharField(max_length=50)
+    price = models.PositiveBigIntegerField()
